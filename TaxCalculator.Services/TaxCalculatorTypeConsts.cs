507b80d80318aae3e9c9b9aedeb50b1a59c454da
sbahle.khawula@platform45.com <sbahle.khawula@platform45.com>
2024-01-29 03:28:43 +0200

    reomed unused class

--- TaxCalculator.Services/TaxCalculatorTypeConsts.cs
+++
@@ -1,6 +0,0 @@
-﻿internal static class TaxCalculatorTypeConsts
-{
-	private const string Progressive = "Progressive";
-	private const string FlatValue = "Flat Value";
-	private const string FlatRate = "Flat Rate";
-}
